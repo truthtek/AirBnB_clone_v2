2fbf0b2595de54b36703bf713941b0921a270d00
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-23 19:35:15 +0100

    Create setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on the hbnb_test_db database to the user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
