4f1775740b8cd9de011ee113a3b978ae52a278df
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-23 19:56:55 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+"""Review module for the HBNB project"""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """Review class to store review information"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Review instance"""
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
+
