25da97abf38a336f6e768c9ef990904f72e85e1e
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-23 19:38:57 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
