8d578c5668a98fd39c44f525263c9644aae7f705
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-04 20:52:48 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 """Fabric script that generates a .tgz archive from the contents of the web_static folder"""
-
 from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
-    try:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        archive_path = 'versions/web_static_{}.tgz'.format(timestamp)
-        local('mkdir -p versions')
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except Exception as e:
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+
+    # create a versions directory if not exists
+    local("mkdir -p versions")
+
+    # create a tgz archive of the web_static directory
+    timestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestr)
+    result = local("tar -cvzf {} web_static".format(archive_path))
+
+    # return the archive path if the archive has been correctly generated. Otherwise, it should return None
+    if result.failed:
         return None
+    else:
+        return archive_path
