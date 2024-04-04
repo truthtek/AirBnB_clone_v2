5668729f7a3a82e0438adfbce2c8131f10cca80d
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-04 20:41:10 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,37 +1,62 @@
 #!/usr/bin/env bash
-# This script sets up web servers for deployment of web_static
+# script to install and setup nginx
+CONFIG_FILE="/etc/nginx/sites-available/default"
+HOST_NAME=$(hostname)
+MY_ID=496
+STATIC=/data/web_static
 
-# Install Nginx if not already installed
-if ! dpkg -s nginx >/dev/null 2>&1; then
-    apt-get update
-    apt-get -y install nginx
+# check if hostname is correct
+if [[ $(hostname) =~ ^$MY_ID-web-[0-9]+ ]]; then
+    echo 'hostname properly configured'
+else
+    (>&2 echo 'hostname not configured properly...')
+    (>&2 echo 'please set hostname to pattern: 496-web-<server_id>...')
+    (>&2 echo 'Example: sudo hostnamectl set-hostname 496-web-<insert_server_id_here>')
 fi
 
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+# install nginx
+apt-get -y update
+apt-get -y install nginx
 
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+# update 404 error page
+echo "Ceci n'est pas une page" > /usr/share/nginx/html/404.html
 
-# Create or recreate symbolic link
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test /data/web_static/current
+# create static directories and links
+mkdir -p $STATIC/releases/test
+mkdir -p $STATIC/shared
+echo 'Holberton School Is Running!' > $STATIC/releases/test/index.html
+ln -sfn $STATIC/releases/test $STATIC/current
+sudo chown -f -R ubuntu:ubuntu /data/
 
-# Give ownership of /data/ folder to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+# update config file to redirect
+printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   $STATIC/current;
+    index  index.html index.htm 8-index.html;
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-sed -i '/^\s*location \/hbnb_static\/ {/a \
-        alias /data/web_static/current/;' "$config_file"
+    add_header X-Served-By $HOST_NAME;
 
-# Restart Nginx
+    location / {
+        alias $STATIC/current/;
+    }
+
+    location /redirect_me {
+        return 301 http://google.com/;
+    }
+
+    location /hbnb_static {
+        alias $STATIC/current/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /usr/share/nginx/html;
+      internal;
+    }
+}" > $CONFIG_FILE
+
+# start nginx after reloading config
+service nginx start
+# if nginx was already running restart it
 service nginx restart
-
-exit 0
