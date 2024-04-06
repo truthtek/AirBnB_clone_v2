2e58ff0d72422b5ab63b972062ca60693dd35085
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-06 21:29:13 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" a Fabric script (based on the file 1-pack_web_static.py) that distributes..
+    ..an archive to your web servers, using the function do_deploy: """
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
