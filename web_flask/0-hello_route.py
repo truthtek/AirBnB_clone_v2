20074badb3ab27e94a8acef96b03d214c8f6a4a8
truthtek <adedokunadebayolukman@gmail.com>
2024-04-21 10:21:50 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!' message"""
     return "Hello HBNB!"
