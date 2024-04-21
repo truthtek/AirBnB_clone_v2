7489bb3a0aab92ae088175365666ee505f9838a1
truthtek <adedokunadebayoukman@gmail.com>
2024-04-21 17:00:21 +0000

    correction

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 
+
+"""utilizing Flask for Web app frame work"""
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Displays 'Hello HBNB!' message"""
+
+@app.route("/")
+def home():
+    """method that defines '/' route for Flask web application"""
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+
+@app.route("/hbnb")
 def hbnb():
-    """Displays 'HBNB' message"""
+    """method that defines '/hbnb' route for Flask web application"""
     return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    app.run(host="localhost", port=5000)
