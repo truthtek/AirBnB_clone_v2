95ec5d0284a5d335433af6f2990cc9dd3b2fc773
truthtek <adedokunadebayoukman@gmail.com>
2024-04-22 16:31:10 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+
+
+"""utilizing Flask for Web app frame work"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def hello():
-    """Displays 'Hello HBNB!' message"""
+
+@app.route("/airbnb-onepage/", strict_slashes = False)
+def home():
+    """method that defines '/' route for Flask web application"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="localhost", port=5000)
